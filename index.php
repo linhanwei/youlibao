495742b5fb52455e50b5824204a896b9da5bc255
linhanwei <jiayuan@unescn.com>
2016-06-28 16:10:52 +0800

    测试

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+//设置默认字符集
+header("Content-type: text/html; charset=utf-8"); 
+
+// 应用入口文件
+
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',TRUE);
+
+//API项目  没生成项目的时候要注释掉,不然只会到默认项目
+// define('BIND_MODULE','Mobile');
+// define('BUILD_MODEL_LIST','User');
+
+// 定义应用目录
+define('APP_PATH','./Uapp/');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
